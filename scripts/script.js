67b667d9ed4a96f2b7dd020f9a300bdd2cb3ce73
RainyKnight <stoltmanlucas@gmail.com>
2021-02-22 15:01:15 -0800

    Route the search to google

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,14 @@ document.getElementById("search").addEventListener("click", myFunction);
 function myFunction() {
     userString = document.getElementById("user-input").value;
     console.log(userString);
+    search(userString);
 }
+
+function search(string) {
+    window.open(`https://www.google.com/search?q=${userString}`);
+}
+
+
+// Add + between each term and add to url
+//https://www.google.com/search?q=(userString)
+//https://www.google.com/search?q=google+search+api
